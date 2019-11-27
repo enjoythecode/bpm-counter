b4fae7c80bc8606513709860a0f6aa9d4cdcd619
Sinan Yumurtacı <sinan.yumurtaci@gmail.com>
2019-11-27 13:10:06 -0500

    clean the code up, refactor variable names for readability

--- bpm.py
+++ bpm.py
@@ -13,9 +13,12 @@ CHANNELS = 1
 RATE = 44100
 RECORD_SECONDS = 1000
 
+RMS_DELTA_THRESHOLD = 1500
+SLIDING_TIMESTAMP_LENGTH = 8
+
 p = pyaudio.PyAudio()
 
-target_bpm = int(input("enter target BPM"))
+target_bpm = int(input("enter target BPM: "))
 target_ms = 60000 / target_bpm
 
 stream = p.open(format=p.get_format_from_width(WIDTH),
@@ -27,46 +30,47 @@ stream = p.open(format=p.get_format_from_width(WIDTH),
 
 print("* recording")
 
-exrms = 0
-DELTA_THRESHOLD = 1500
-start_ms = current_milli_time()
-count = 0
-last8 = []
+past_rms = 0
+beat_count = 0
+sliding_timestamps = []
+
 all_bpm = []
-all_ms = []
-delta_ms = []
+all_timestamps = []
+all_delta = []
+
+start_ms = current_milli_time()
 
 for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
     data = stream.read(CHUNK)
     data = np.array(wave.struct.unpack("%dh"%(len(data)/WIDTH), data))*2
     
-    rms = audioop.rms(data,2)
-    if(rms > exrms + DELTA_THRESHOLD):
+    curr_rms = audioop.rms(data,2)
+    if(curr_rms > past_rms + RMS_DELTA_THRESHOLD):
 
-        count += 1
+        beat_count += 1
         now_ms = current_milli_time()
         time_ms = now_ms - start_ms
-        last8.append(time_ms)
-        all_ms.append(now_ms)
-        if(len(all_ms) > 1):
-            delta_ms.append(all_ms[-1] - all_ms[-2])
+        sliding_timestamps.append(time_ms)
+        all_timestamps.append(now_ms)
+        if(len(all_timestamps) > 1):
+            all_delta.append(all_timestamps[-1] - all_timestamps[-2])
 
-        
-        if(len(last8) > 8):
-            last8 = last8[1:9]
+        if(len(sliding_timestamps) > SLIDING_TIMESTAMP_LENGTH):
+            sliding_timestamps = sliding_timestamps[1:SLIDING_TIMESTAMP_LENGTH+1]
 
-        print("count", count)
-        if(len(last8) == 8):
-            print((last8[7] - last8[0]))
-            bpm = 7/((last8[7] - last8[0])/1000) * 60
+        print("*"*10)
+        print("beat_count", beat_count)
+        if(len(sliding_timestamps) == SLIDING_TIMESTAMP_LENGTH):
+            print(all_delta[-1])
+            bpm = (SLIDING_TIMESTAMP_LENGTH - 1)/((sliding_timestamps[SLIDING_TIMESTAMP_LENGTH - 1] - sliding_timestamps[0])/1000) * 60
             all_bpm.append(bpm)
             
-            print("bpm", all_bpm[-1])
-            print("ms diff from target", delta_ms[-1] - target_ms)
-            print("ms-bpm", 60000 / delta_ms[-1])
+            print("bpm over last " + str(SLIDING_TIMESTAMP_LENGTH) + "beats: " , all_bpm[-1])
+            print("ms diff from target: ", all_delta[-1] - target_ms)
+            print("ms-bpm: ", 60000 / all_delta[-1])
 
 
-    exrms = rms
+    past_rms = curr_rms
 
 print("* done")
 stream.stop_stream()
