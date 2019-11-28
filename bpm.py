2cf2a074a667cae8e994cbec0971acc6e4b9db11
Sinan Yumurtacı <sinan.yumurtaci@gmail.com>
2019-11-27 22:35:54 -0500

    polish before pushing to public

--- bpm.py
+++ bpm.py
@@ -1,3 +1,12 @@
+# Author: Sinan Yumurtaci -- sinan@enjoythecode.com
+# Date: 11/27/2019
+# Purpose: "Calculate" BPM from microphone by detecting sudden rises in sound loudness and registering the time between each "beat."
+# 
+# Useful making sure you aren't going too fast/slow.
+# Works best with BPM < 300, and in quiet environments. Can tolerate constant noise for the most part (e.g. fan)
+#
+# Complimentary to my drumming hobby. Enjoy!
+
 import pyaudio
 import numpy as np
 import wave
@@ -11,10 +20,10 @@ CHUNK = 1024
 WIDTH = 2
 CHANNELS = 1
 RATE = 44100
-RECORD_SECONDS = 1000
+RECORD_SECONDS = 1000 # the program runs for this many seconds. set to a high number for indefinite sessions
 
-RMS_DELTA_THRESHOLD = 1500
-SLIDING_TIMESTAMP_LENGTH = 8
+RMS_DELTA_THRESHOLD = 5 # how much of a change in sound registers as a beat. Higher values are more insensitive and vice versa
+SLIDING_TIMESTAMP_LENGTH = 8 # calculates BPM using last SLIDING_TIMESTAMP_LENGTH beats
 
 p = pyaudio.PyAudio()
 
@@ -28,7 +37,6 @@ stream = p.open(format=p.get_format_from_width(WIDTH),
                 output=True,
                 frames_per_buffer=CHUNK)
 
-print("* recording")
 
 past_rms = 0
 beat_count = 0
@@ -40,42 +48,47 @@ all_delta = []
 
 start_ms = current_milli_time()
 
+print("***** == RECORDING == *****")
+
 for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
     data = stream.read(CHUNK)
     data = np.array(wave.struct.unpack("%dh"%(len(data)/WIDTH), data))*2
     
     curr_rms = audioop.rms(data,2)
-    if(curr_rms > past_rms + RMS_DELTA_THRESHOLD):
+
+    # is there a beat?
+    if(curr_rms > max(past_rms, 400) * RMS_DELTA_THRESHOLD):
 
         beat_count += 1
         now_ms = current_milli_time()
         time_ms = now_ms - start_ms
         sliding_timestamps.append(time_ms)
         all_timestamps.append(now_ms)
+
+        # start calculating "delta" (time between beats) from the second beat on
         if(len(all_timestamps) > 1):
             all_delta.append(all_timestamps[-1] - all_timestamps[-2])
 
+        # ensure the sliding timestamps array is not too long
         if(len(sliding_timestamps) > SLIDING_TIMESTAMP_LENGTH):
             sliding_timestamps = sliding_timestamps[1:SLIDING_TIMESTAMP_LENGTH+1]
 
         print("*"*10)
-        print("beat_count", beat_count)
+        print("beat #", beat_count)
         if(len(sliding_timestamps) == SLIDING_TIMESTAMP_LENGTH):
             print(all_delta[-1])
             bpm = (SLIDING_TIMESTAMP_LENGTH - 1)/((sliding_timestamps[SLIDING_TIMESTAMP_LENGTH - 1] - sliding_timestamps[0])/1000) * 60
             all_bpm.append(bpm)
             
-            print("bpm over last " + str(SLIDING_TIMESTAMP_LENGTH) + "beats: " , all_bpm[-1])
-            print("ms diff from target: ", all_delta[-1] - target_ms)
-            print("ms-bpm: ", 60000 / all_delta[-1])
+            print("BPM over last " + str(SLIDING_TIMESTAMP_LENGTH) + " beats: " , all_bpm[-1])
+            print("millisecond difference from target: ", all_delta[-1] - target_ms)
+            print("BPM @ last millisecond delay: ", 60000 / all_delta[-1])
 
 
     past_rms = curr_rms
 
-print("* done")
+print("***** == DONE == *****")
+
 stream.stop_stream()
 stream.close()
-
 p.terminate()
-
-
